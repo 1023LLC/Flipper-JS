5b420aad3eeb7cbee4b8211b5947a1420330352f
julius <nyerereoffice@gmail.com>
2024-03-29 23:18:34 +0300

    Final commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const body = document.getElementsByTagName('body')[0]
+
+
+function setColor(name){
+    body.style.backgroundColor = name;
+}
+
+
+function randomColor(){
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const tagElement = document.getElementById('tag')
+
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+
+    tagElement.innerHTML = "The generated color is: " + color
+
+    body.style.backgroundColor = color;
+
+
+}
+
+
+randomColor();
